40673dcf151bfb98af80d06b888af31eff1a9857
Filip Bojovic <fikakv@gmail.com>
2024-01-09 22:10:53 +0100

    Feature: Sec-13- finished the course

---
+++ Understanding-TypeScript-2023-Edition/sec-13-practice-project/src/app.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput: HTMLInputElement = document.getElementById("address")! as HTMLInputElement;
+const GOOGLE_API_KEY = "dfgdfgdfgdg";
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
